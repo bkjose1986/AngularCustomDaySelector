59c0b185f6883e0e988545ba379241585b7266f1
Bijoy KJ <bkjose1986@gmail.com>
2018-10-14 15:06:54 +0530

    Adding the module

---
+++ test-app/src/dayselector/daySelector.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Angular imports
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl : './daySelector.component.html',
+    styleUrls : ['./daySelector.component.css'],
+    // tslint:disable-next-line:component-selector
+    selector : 'day-selector'
+})
+export class DaySelectorComponent {
+
+}
---
+++ test-app/src/dayselector/daySelector.module.ts
@@ -0,0 +1,19 @@
+/**
+ * Angular imports
+ */
+import { NgModule } from '@angular/core';
+/**
+ * Components
+ */
+import { DaySelectorComponent } from './daySelector.component';
+
+@NgModule({
+    imports: [],
+    declarations : [ DaySelectorComponent ],
+    exports : [ DaySelectorComponent ],
+    providers : [],
+    entryComponents : []
+
+})
+export class DaySelectorModule {}
+
