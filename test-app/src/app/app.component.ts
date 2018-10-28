74c7fba47dbc395527060b3928556b235650c0bf
Bijoy KJ <bkjose1986@gmail.com>
2018-10-28 16:23:33 +0530

    Code cleanup

--- test-app/src/app/app.component.ts
+++ test-app/src/app/app.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'test-app';
   countChangedHandler(count: number) {
-   
-    console.log(count);
+        console.log(count);
   }
 }
